4fd46a913fdb3aa92cb6fc4207f6128f64ca75d1
Aleem Haji <hajial@gmail.com>
2015-07-02 23:17:53 -0400

    Made all tests a little more succinct.

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,31 @@
 var request = require("request");
 var server = require("./testServer");
 
+function testRoute(test, route, method, json, headers, expectStatusCode, expectBody)
+{
+	var testCount = 2 + (expectBody != null ? 2 : 0);
+	test.expect(testCount);
+	request({uri: "http://localhost:8080/validation" + route, method: method, json: json, headers: headers}, function(err, resp, body) {
+		if ( typeof body === "string" && body.length > 0 ) { // Request is a finicky package.
+			body = JSON.parse(body);
+		}
+		test.ifError(err);
+		test.equal(resp.statusCode, expectStatusCode);
+		if ( expectBody ) {
+			test.ok(body)
+			test.deepEqual(body, expectBody);
+		}
+		test.done();
+	});
+}
+
+function makeTest(route, method, json, headers, expectStatusCode, expectBody)
+{
+	return function(test) {
+		testRoute(test, route, method, json, headers, expectStatusCode, expectBody)
+	}
+}
+
 module.exports = {
     setUp: function(done) {
         server.listen(8080, function() {
@@ -10,67 +35,29 @@ module.exports = {
         });
     },
     tearDown: function(done) {
-        server.close();
-        done();
-    },
-    testNoValidation: function(test) {
-        test.expect(1);
-        request.get("http://localhost:8080/validation/none", function(err, resp, body) {
-            test.equal(resp.statusCode, 200);
-            test.done();
+        server.close(function() {
+	        done();
         });
     },
-    testRequiredBodyFieldValidation: function(test) {
-        test.expect(4);
-        request({uri: "http://localhost:8080/validation/body/required", method: "PUT", json: { "a": 1, "b": 2, "c": 3}}, function(err, resp, body) {
-            test.equal(resp.statusCode, 200);
-            request({uri: "http://localhost:8080/validation/body/required", method: "PUT"}, function(err, resp, body) {
-                test.equal(resp.statusCode, 403);
-                request({uri: "http://localhost:8080/validation/body/required", method: "PUT", json: { "a": 1, "c": 3 }}, function(err, resp, body) {
-                    test.equal(resp.statusCode, 403);
-                    test.deepEqual(body, { code: "ForbiddenError", message: "Must send fields: (b)" });
-                    test.done();
-                });
-            });
-        });
+    testNoValidation: makeTest("/none", "GET", null, null, 200),
+    testRequiredBodyFieldValidation: {
+    	testSuccess: makeTest("/body/required", "PUT", { "a": 1, "b": 2, "c": 3}, null, 200),
+    	testFailure: makeTest("/body/required", "PUT", { "a": 1, "c": 3}, null, 403, { code: "ForbiddenError", message: "Must send fields: (b)" }),
+    	testNoBody: makeTest("/body/required", "PUT", null, null, 403, { code: "ForbiddenError", message: "Must send fields: (a,b)" })
     },
-    testRestrictedBodyFieldValidation: function(test) {
-        test.expect(4);
-        request({uri: "http://localhost:8080/validation/body/restricted", method: "PUT"}, function(err, resp, body) {
-            test.equal(resp.statusCode, 200);
-            request({uri: "http://localhost:8080/validation/body/restricted", method: "PUT", json: { "a": 1, "b": 2 }}, function(err, resp, body) {
-                test.equal(resp.statusCode, 200);
-                request({uri: "http://localhost:8080/validation/body/restricted", method: "PUT", json: { "a": 1, "b": 3, "d": 4}}, function(err, resp, body) {
-                    test.equal(resp.statusCode, 403);
-                    test.deepEqual(body, { code: "ForbiddenError", message: "Cannot send fields: (d)" });
-                    test.done();
-                });
-            });
-        });
+    testRestrictedBodyFieldValidation: {
+    	testSuccess: makeTest("/body/restricted", "PUT", { "a": 1, "b": 2}, null, 200),
+    	testFailure: makeTest("/body/restricted", "PUT", { "a": 1, "b": 2, "d": 4}, null, 403, { code: "ForbiddenError", message: "Cannot send fields: (d)" }),
+    	testNoBody: makeTest("/body/restricted", "PUT", null, null, 200)
     },
-    testPermittedBodyFieldValidation: function(test) {
-        test.expect(4);
-        request({uri: "http://localhost:8080/validation/body/permitted", method: "PUT", json: { "a": 1, "b": 2 }}, function(err, resp, body) {
-            test.equal(resp.statusCode, 200);
-            request({uri: "http://localhost:8080/validation/body/permitted", method: "PUT"}, function(err, resp, body) {
-                test.equal(resp.statusCode, 200);
-                request({uri: "http://localhost:8080/validation/body/permitted", method: "PUT", json: { "a": 1, "b": 3, "f": 5, "g": 9}}, function(err, resp, body) {
-                    test.equal(resp.statusCode, 403);
-                    test.deepEqual(body, { code: "ForbiddenError", message: "Cannot send fields: (f,g)" });
-                    test.done();
-                });
-            });
-        });
+    testPermittedBodyFieldValidation: {
+    	testSuccess: makeTest("/body/permitted", "PUT", { "a": 1, "b": 2}, null, 200),
+    	testFailure: makeTest("/body/permitted", "PUT", { "a": 1, "b": 2, "f": 6, "g": 7}, null, 403, { code: "ForbiddenError", message: "Cannot send fields: (f,g)" }),
+    	testNoBody: makeTest("/body/permitted", "PUT", null, null, 200)
     },
-    testHeaderValidation: function(test) {
-        test.expect(3);
-        request({uri: "http://localhost:8080/validation/headers/required", method: "PUT", headers: { "x-application-key": 1, "x-client-id": 2 }}, function(err, resp, body) {
-            test.equal(resp.statusCode, 200);
-            request({uri: "http://localhost:8080/validation/headers/required", method: "PUT", json: { "a": 1, "b": 3, "f": 5, "g": 9}}, function(err, resp, body) {
-                test.equal(resp.statusCode, 403);
-                test.deepEqual(body, { code: "ForbiddenError", message: "Must send headers: (x-application-key,x-client-id)" });
-                test.done();
-            });
-        });
+    testHeaderValidation: {
+      	testSuccess: makeTest("/headers/required", "PUT", null, { "x-application-key": 1, "x-client-id": 2 }, 200),
+    	testFailure: makeTest("/headers/required", "PUT", null, { "x-application-key": 1 }, 403, { code: "ForbiddenError", message: "Must send headers: (x-client-id)" }),
+    	testNoHeaders: makeTest("/headers/required", "PUT", null, null, 403, { code: "ForbiddenError", message: "Must send headers: (x-application-key,x-client-id)" })
     }
 }
