795615b5622cf3058f4222be5574bf09732d95cd
Aleem Haji <hajial@gmail.com>
2015-07-02 22:43:02 -0400

    Replaced a bunch of tabs with spaces.

--- tests/testServer.js
+++ tests/testServer.js
@@ -18,39 +18,39 @@ lumen.use("restrictedBodyFields", lumina.restrictedBodyFieldValidator());
 lumen.use("permittedBodyFields", lumina.permittedBodyFieldValidator());
 
 function defaultHandler(req, res, next) {
-	res.send(200);
-	return next();
+    res.send(200);
+    return next();
 }
 
 var routes = [{
-	method: "get",
-	path: "/validation/none"
+    method: "get",
+    path: "/validation/none"
 }, {
-	method: "put",
-	path: "/validation/body/required",
-	requiredBodyFields: ["a", "b"]
+    method: "put",
+    path: "/validation/body/required",
+    requiredBodyFields: ["a", "b"]
 }, {
-	method: "put",
-	path: "/validation/body/restricted",
-	restrictedBodyFields: ["d", "e"]
+    method: "put",
+    path: "/validation/body/restricted",
+    restrictedBodyFields: ["d", "e"]
 }, {
-	method: "put",
-	path: "/validation/body/permitted",
-	permittedBodyFields: ["a", "b"]
+    method: "put",
+    path: "/validation/body/permitted",
+    permittedBodyFields: ["a", "b"]
 }, {
-	method: "put",
-	path: "/validation/headers/required",
-	requiredHeaders: ["x-application-key", "x-client-id"]
+    method: "put",
+    path: "/validation/headers/required",
+    requiredHeaders: ["x-application-key", "x-client-id"]
 }]
 
 for ( var i = 0; i < routes.length; ++i ) {
-	var d = routes[i];
-	var m = d.method;
-	var r = d.path;
-	delete d.method
-	delete d.path;
-	d.handler = defaultHandler;
-	server[m](r, lumen.illuminate(d));
+    var d = routes[i];
+    var m = d.method;
+    var r = d.path;
+    delete d.method
+    delete d.path;
+    d.handler = defaultHandler;
+    server[m](r, lumen.illuminate(d));
 }
 
 module.exports = server;
